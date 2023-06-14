1bbc95ed4a6d30b8a57a477587886f53299537e5
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-14 09:41:19 +0530

    modularity

---
+++ src/pages/AppFooter.js
@@ -0,0 +1,16 @@
+
+// User Defined Tag: <AppFooter />
+function AppFooter() {
+    return (
+      <>
+        <div
+          className="bg-dark text-light d-flex justify-content-center align-items-center"
+          style={{ height: "200px" }}
+        >
+          Lorem ipsum dolor sit amet.
+        </div>
+      </>
+    );
+  }
+
+  export default AppFooter;
